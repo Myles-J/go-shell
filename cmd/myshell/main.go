f9406dd58cfe6db9b97e1016605174f322f998a0
Myles Jefferson <99mylesjefferson@gmail.com>
2024-12-07 12:43:13 -0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,61 +4,91 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
+type (
+	cmdFnc func([]string)
+)
+
+var commands = make(map[string]cmdFnc)
+
+func registerCommand(cmd string, fn cmdFnc) {
+	commands[cmd] = fn
+}
+
+func exit(args []string) {
+	if len(args) == 0 {
+		os.Exit(1)
+	}
+	if code, err := strconv.Atoi(args[0]); err == nil {
+		os.Exit(code)
+	}
+}
+
+func typer(args []string) {
+	if len(args) == 0 {
+		fmt.Println("")
+	}
+
+	_, builtin := commands[args[0]]
+
+	if builtin {
+		fmt.Printf("%s is a shell builtin\n", args[0])
+		return
+	}
+
+	paths := strings.Split(os.Getenv("PATH"), ":")
+
+	for _, path := range paths {
+		fp := filepath.Join(path, args[0])
+		if _, err := os.Stat(fp); err == nil {
+			fmt.Println(fp)
+			return
+		}
+	}
+	fmt.Printf("%s: not found\n", args[0])
+}
+
+func echo(args []string) {
+	phrase := strings.Join(args, " ")
+	fmt.Fprintf(os.Stdout, "%s\n", phrase)
+}
+
+func notFound(cmd string) {
+	fmt.Printf("%s: command not found\n", cmd)
+}
+
+func initCommands() {
+	registerCommand("echo", echo)
+	registerCommand("exit", exit)
+	registerCommand("type", typer)
+}
+
 func main() {
-
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-	f := bufio.NewReader(os.Stdin)
-
+	initCommands()
 	for {
-		command, err := f.ReadString('\n')
-		command = strings.Trim(command, "\n")
+		fmt.Fprint(os.Stdout, "$ ")
+
+		// Wait for user input
+		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			fmt.Println(err)
+			fmt.Printf("error reading from stdin: %s", err.Error())
 			os.Exit(1)
 		}
 
-		splitCommand := strings.Split(command, " ")
+		inputs := strings.Split(strings.TrimSpace(in), " ")
+		cmd := inputs[0]
+		args := inputs[1:]
 
-		if splitCommand[0] == "echo" {
-			echoPhrase := strings.Join(splitCommand[1:], " ")
-			fmt.Fprintf(os.Stdout, "%s\n", echoPhrase)
-			fmt.Fprint(os.Stdout, "$ ")
-			continue
+		cmdFn, ok := commands[cmd]
+
+		if !ok {
+			notFound(cmd)
+		} else {
+			cmdFn(args)
 		}
-
-		if splitCommand[0] == "type" {
-			kwargs := map[string]string{
-				"help": "help",
-				"exit": "exit", 
-				"echo": "echo",
-				"type": "type",
-			}
-
-			val, ok := kwargs[splitCommand[1]]
-			if ok {
-				fmt.Fprintf(os.Stdout, "%s is a shell builtin\n", val)
-				fmt.Fprint(os.Stdout, "$ ")
-				continue
-			} else {
-				fmt.Fprintf(os.Stdout, "%s: not found\n", splitCommand[1])
-				fmt.Fprint(os.Stdout, "$ ")
-				continue
-			}
-
-		}
-
-		if command == "exit 0" {
-			os.Exit(0)
-		}
-
-		fmt.Fprintf(os.Stdout, "%s: command not found\n", command)
-		fmt.Fprint(os.Stdout, "$ ")
-
 	}
-
 }
